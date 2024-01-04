be64256d79cdfcbf7132379061a75b2f304d97ad
Aristec1 <mariston748@gmail.com>
2024-01-04 20:46:43 -0300

    Segundo e ultimo commit

--- desafio_2.md
+++ desafio_2.md
@@ -10,7 +10,7 @@
 ## Objetivo:
 
 Criar uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retornar o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+depois disso deve retornar o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
 
 Se vitórias for menor do que 10 = Ferro
 Se vitórias for entre 11 e 20 = Bronze
--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-//Criar ainda
+//Definindo a variável de saída e chamando a função saldo para ser executada.
+let saida = saldo(100, 9)
+
+//Aplicando as variáveis para a estrutura de decisão escolher o nível de classificação de acordo com o resultado da função saldo.
+let xp = saida
+let nivel
+if (xp < 10){
+    nivel = "Ferro"
+} else if (xp <= 20){
+    nivel = "Bronze"
+} else if (xp <= 50){
+    nivel = "prata"
+} else if (xp <= 80){
+    nivel = "Ouro"
+} else if (xp <= 90){
+    nivel = "Diamante"
+} else if (xp <= 100){
+    nivel = "Lendário"
+} else if (xp >= 101){
+    nivel = "Imortal"
+}
+
+//Resultado sendo mostrado
+console.log("O heroi tem saldo de " + saida + ", e está no nível de " + nivel)
+//Função de saldo com o cálculo da diferença entre vitórias e derrotas.
+
+function saldo(vitoria, derrota){
+    let resultado = (vitoria - derrota)
+    return resultado
+}
